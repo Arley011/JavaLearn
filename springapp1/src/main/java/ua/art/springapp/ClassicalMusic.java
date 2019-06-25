1cd6412c141f743e73e61a811e55ccc7f0600bbc
Arley011 <arturleyza5@gmail.com>
2019-06-25 23:23:38 +0300

    Spring: factory-method

--- springapp1/src/main/java/ua/art/springapp/ClassicalMusic.java
+++ springapp1/src/main/java/ua/art/springapp/ClassicalMusic.java
@@ -2,6 +2,12 @@ package ua.art.springapp;
 
 public class ClassicalMusic implements Music {
 
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
