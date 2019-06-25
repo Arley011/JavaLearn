0cfe978d8935a7fbd336494ed9dc01f6a853e256
Arley011 <arturleyza5@gmail.com>
2019-06-25 20:27:39 +0300

    Spring: Init/destroy methods

--- springapp1/src/main/java/ua/art/springapp/ClassicalMusic.java
+++ springapp1/src/main/java/ua/art/springapp/ClassicalMusic.java
@@ -6,4 +6,12 @@ public class ClassicalMusic implements Music {
     public String getSong() {
         return "Hungarian Rhapsody";
     }
+
+    public void doMyInit() {
+        System.out.println("Doing initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing destruction");
+    }
 }
