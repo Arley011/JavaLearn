8e716988d839ee50e04794fa6043e8c856bb41a1
Arley011 <arturleyza5@gmail.com>
2019-06-25 19:37:30 +0300

    Spring: Music player without list of beans

--- springapp1/src/main/java/ua/art/springapp/MusicPlayer.java
+++ springapp1/src/main/java/ua/art/springapp/MusicPlayer.java
@@ -4,20 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MusicPlayer {
-    private List<Music> musicList = new ArrayList<>();
+    private Music music;
     private String name;
     private int volume;
 
-    public MusicPlayer(List<Music> musicList){
-        this.musicList = musicList;
+    public MusicPlayer(Music music){
+        this.music = music;
     }
     public MusicPlayer(){}
 
 
     public void playMusic(){
-        for(Music music : musicList){
             System.out.println("Playing: "+ music.getSong());
-        }
+
     }
 
     public String getName() {
@@ -36,7 +35,7 @@ public class MusicPlayer {
         this.volume = value;
     }
 
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
+    public void setMusic(Music music){ this.music = music; }
+
+
 }
